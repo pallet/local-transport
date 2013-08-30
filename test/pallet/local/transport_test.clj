14319c14575c9822555ee3a94ccc4dc9fdafd949
Hugo Duncan <hugo@hugoduncan.org>
2013-08-29 20:15:15 -0400

    Break out components of the command map
    This allows more freedom for the implementation to process the
    command map.  Require for proper environment forwarding.

--- test/pallet/local/transport_test.clj
+++ test/pallet/local/transport_test.clj
@@ -14,15 +14,19 @@
     (let [result (transport/exec {:in "ls /; exit $?"} nil)]
       (is (zero? (:exit result)))
       (is (re-find #"bin" (:out result)))))
-  ;; this fails for ssh for some reason
-  ;; (testing "Explicit shell"
-  ;;   (let [result (transport/exec
-  ;;                 t-state
-  ;;                 {:execv ["/bin/bash"] :in "ls /; exit $?"} nil)]
-  ;;     (is (zero? (:exit result)))
-  ;;     (is (re-find #"bin" (:out result)))))
+  (testing "Explicit shell"
+    (let [result (transport/exec
+                  {:execv ["/bin/bash"] :in "ls /; exit $?"} nil)]
+      (is (zero? (:exit result)))
+      (is (re-find #"bin" (:out result)))))
   (testing "Explicit program"
     (let [result (transport/exec {:execv ["/bin/ls" "/"]} nil)]
+      (is (zero? (:exit result)))
+      (is (re-find #"bin" (:out result)))))
+  (testing "prefix"
+    (let [result (transport/exec
+                  {:prefix ["/usr/bin/env"] :execv ["/bin/ls" "/"]}
+                  nil)]
       (is (zero? (:exit result)))
       (is (re-find #"bin" (:out result)))))
   (testing "output-f"
@@ -35,7 +39,35 @@
       (is (= (:out result) @output))))
   (testing "Error return"
     (let [result (transport/exec {:in "this-should-fail"} nil)]
-      (is (not (zero? (:exit result)))))))
+      (is (not (zero? (:exit result))))))
+  (testing "Env"                        ; if this fails so will the next test
+                                        ; as LEIN_VERSION is not in the env
+      (let [result (transport/exec
+                    {:execv ["/usr/bin/env"]}
+                    {:agent-forwarding true})]
+        (is (zero? (:exit result)))
+        (is (re-find
+             (re-pattern (System/getenv "LEIN_VERSION"))
+             (:out result)))))
+  (testing "Explicit env"
+    (let [tmp (java.io.File/createTempFile "pallet_" "tmp")]
+      (.deleteOnExit tmp)
+      (spit tmp "echo ${XX}${LEIN_VERSION}")
+      (try
+        (let [result (transport/exec
+                      {:prefix ["/usr/bin/sudo"]  ; restrict vars
+                       :execv ["/bin/bash" (.getAbsolutePath tmp)]
+                       :env-cmd "/usr/bin/env"
+                       :env-fwd [:LEIN_VERSION]
+                       :env {:XX "abcd"}}
+                      nil)]
+          (is (zero? (:exit result)))
+          (is (re-find
+               (re-pattern (str "abcd" (System/getenv "LEIN_VERSION")))
+               (:out result))))
+        (catch Exception e
+          (.delete tmp)
+          (throw e))))))
 
 (deftest test-send
   (testing "send-stream"
